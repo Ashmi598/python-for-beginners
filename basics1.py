640cae1eb318bb5bd743993947d476f92de63b3e
Ashmi Teresa Alfred <64002969+Ashmi598@users.noreply.github.com>
2020-08-28 23:16:19 +0530

    Add files via upload

---
+++ basics1.py
@@ -0,0 +1,5 @@
+#program for the following condition
+#ask two questions : person's name and favourite colour.Then , print a message like "Mosh like blue"
+name = input ("What is your name?")
+fav_colour = input("What is your favourite colour?")
+print(name + 'likes' + fav_colour)
