cbb8c4ff58ae67f5001b271167490f3b2afbd905
Ashmi Teresa Alfred <64002969+Ashmi598@users.noreply.github.com>
2020-08-28 23:02:37 +0530

    Add files via upload

---
+++ basics0.py
@@ -0,0 +1,20 @@
+print("Ashmi_Alfred")
+print('o-------')
+print('////')
+print('*' * 9)
+
+
+price = 30
+rating= 5.6
+name = "Ash"
+is_pubhlished = True
+print (price)
+
+#writing a program for the following condition
+#we check in a patient named John Smith.He is 20 years old and is a new patient
+name = 'John Smith'
+age = 20
+is_new = True
+
+
+
